9acb3aed1b99314e54bf0b234ee9d24fb8e1a59f
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-02-07 15:57:15 -0300

    mensagem inicial

--- jogo da velha.c
+++ jogo da velha.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#define x 1
+#define o 2
 
 int main() {
     
+    printf("Ola bem vindo a jogo da velha\n");
+    printf("\t   |   |   \n");
+    printf("\t-----------\n");
+    printf("\t   |   |   \n");
+    printf("\t-----------\n");
+    printf("\t   |   |   \n");
+    
+    printf("\t 1 | 2 | 3 \n");
+    printf("\t-----------\n");
+    printf("\t 4 | 5 | 6 \n");
+    printf("\t-----------\n");
+    printf("\t 7 | 8 | 9 \n");
+	
 }
