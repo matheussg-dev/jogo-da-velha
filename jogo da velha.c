930944de1053c265831e160079ecc687e4226fa1
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-02-07 16:23:58 -0300

    mensagem inicial final

--- jogo da velha.c
+++ jogo da velha.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
+
 #define x 1
 #define o 2
+#define vazio 0
 
 int main() {
     
-    printf("Ola bem vindo a jogo da velha\n");
+int start;
+
+    printf("Ola bem vindo a jogo da velha.\n");
     printf("\t   |   |   \n");
     printf("\t-----------\n");
     printf("\t   |   |   \n");
     printf("\t-----------\n");
     printf("\t   |   |   \n");
+    
+    printf("Aperte qualquer tecla para iniciar.\n");
+    scanf("%i", &start);
     
     printf("\t 1 | 2 | 3 \n");
     printf("\t-----------\n");
     printf("\t 4 | 5 | 6 \n");
     printf("\t-----------\n");
     printf("\t 7 | 8 | 9 \n");
-	
+    
 }
