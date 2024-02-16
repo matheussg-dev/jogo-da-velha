af058813764d2447352ff2b09a4128e4fa88d352
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-02-16 18:10:57 -0300

    posiçao e definindo os jogadores

--- jogo da velha.c
+++ jogo da velha.c
@@ -3,46 +3,98 @@
 #define x 1
 #define o 2
 #define vazio 0
+char posicaoJogo[3][3];
+
+
+//protopitagem de metodo
+void letreiro();
+void entradaMatriz(int push, char elemento);
+
 
 int main() {
     
-    int start;
-    int l;
+    int posicao;
+    int controle = 1;
     int c;
-    char posiçaoJogo[3][3];
+    int l;
+  
+    letreiro();
+    while(1) {
+        if(controle %2 == 0) {
+            printf("Jogador O informe a posicao.\n");
+            scanf("%d", &posicao);
+            controle --;
+            entradaMatriz(posicao, 'O');
+        }
+        else {
+            printf("Jogador x informe a posicao.\n");
+            scanf("%d", &posicao);
+            controle ++;
+            entradaMatriz(posicao, 'X');
+        }
 
+        //matriz do jogo da velha
+        for(l = 0; l < 3; l++) {
+            for(c = 0; c < 3; c++) {
+                printf ("%c", posicaoJogo[l][c]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+void letreiro() {
+    
     //mensagem inicial
 
-    printf("Ola bem vindo a jogo da velha.\n");
+    printf("Ola bem vindo a jogo da velha.\n\n");
     printf("\t   |   |   \n");
     printf("\t-----------\n");
     printf("\t   |   |   \n");
     printf("\t-----------\n");
     printf("\t   |   |   \n");
-    
-    printf("Aperte qualquer tecla para iniciar.\n");
-    scanf("%i", &start);
     
     printf("\t 1 | 2 | 3 \n");
     printf("\t-----------\n");
     printf("\t 4 | 5 | 6 \n");
     printf("\t-----------\n");
     printf("\t 7 | 8 | 9 \n");
+}
 
-    //matriz do jogo da velha
-    for(l = 0; l < 3; l++) {
-        for(c = 0; c < 3; c++) {
-            posiçaoJogo[l][c] = ' ';
-        }
+void entradaMatriz(int push, char elemento) {
+    if (push == 1) {
+        posicaoJogo[0][0] = elemento;
     }
     
-    //imprimir a matriz
-    for(l = 0; l < 3; l++) {
-        for(c = 0; c < 3; c++) {
-            printf("%c", posiçaoJogo[l][c]);
-        }
-        printf("\n");
+    if (push == 2) {
+        posicaoJogo[0][1] = elemento;
     }
     
-    return(0);
+    if (push == 3) {
+        posicaoJogo[0][2] = elemento;
+    }
+    
+    if (push == 4) {
+        posicaoJogo[1][0] = elemento;
+    }
+    
+    if (push == 5) {
+        posicaoJogo[1][1] = elemento;
+    }
+    
+    if (push == 6) {
+        posicaoJogo[1][2] = elemento;
+    }
+    
+    if (push == 7) {
+        posicaoJogo[2][0] = elemento;
+    }
+    
+    if (push == 8) {
+        posicaoJogo[2][1] = elemento;
+    }
+    
+    if (push == 9) {
+        posicaoJogo[2][2] = elemento;
+    }
 }
