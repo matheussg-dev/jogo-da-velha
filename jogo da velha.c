8263f7e94125e0c9c8a5138e5334d2f217479384
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-02-21 17:35:44 -0300

    validaçao horizontal

--- jogo da velha.c
+++ jogo da velha.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
+#define vazio _
 #define x 1
 #define o 2
+
 
 char posicaoJogo[3][3];
 
@@ -11,7 +14,7 @@ void mensagemIniciar();
 void letreiro();
 void entradaMatriz(int push, char elemento);
 void escolhaIniciar();
-
+int validar(char jogada);
 
 int main() {
     
@@ -23,8 +26,8 @@ int main() {
 
 void mensagemIniciar() {
     
-    printf("Ola bem vindo a jogo da velha v0.2.1 .\n");
-    printf("Feito em linguagem C\n\n");
+    printf("\tOla bem vindo a jogo da velha v0.3 .\n");
+    printf("\tFeito em linguagem C\n\n");
     printf("\t   |   |   \n");
     printf("\t-----------\n");
     printf("\t   |   |   \n");
@@ -59,28 +62,32 @@ void escolhaIniciar() {
 
 void letreiro() {
     
+    char xo;
     int posicao;
     int controle = 1;
     int coluna;
     int linha;
     
-    printf("\t 1 | 2 | 3 \n");
-    printf("\t-----------\n");
-    printf("\t 4 | 5 | 6 \n");
-    printf("\t-----------\n");
-    printf("\t 7 | 8 | 9 \n");
+    printf("\t\t 1 | 2 | 3 \n");
+    printf("\t\t-----------\n");
+    printf("\t\t 4 | 5 | 6 \n");
+    printf("\t\t-----------\n");
+    printf("\t\t 7 | 8 | 9 \n\n");
     
     while(1) {
         if(controle %2 == 0) {
-            printf("Jogador O informe a posicao.\n\n");
+            printf("Jogador O informe a posicao.\n");
             scanf("%d", &posicao);
             controle --;
-            entradaMatriz(posicao, 'O');
+            xo = 'O';
+            entradaMatriz(posicao, xo);
         } else {
-            printf("Jogador x informe a posicao.\n\n");
+            printf("Jogador x informe a posicao.\n");
             scanf("%d", &posicao);
             controle ++;
-            entradaMatriz(posicao, 'X');
+            xo = 'X';
+            entradaMatriz(posicao, xo);
+            
         }
 
         //matriz do jogo da velha
@@ -90,43 +97,60 @@ void letreiro() {
             }
             printf("\n");
         }
+        
+        if(validar(xo) == 1) {
+            printf("parabens jogado %c venceu", xo);
+            break;
+        }
     }
 }
 
 void entradaMatriz(int push, char elemento) {
-    if (push == 1) {
-        posicaoJogo[0][0] = elemento;
+    
+    if(!(1 >= push && push <= 9)) {
+        printf("\tposicao inválida.\n\n");
+        printf("\t 1 | 2 | 3 \n");
+        printf("\t-----------\n");
+        printf("\t 4 | 5 | 6 \n");
+        printf("\t-----------\n");
+        printf("\t 7 | 8 | 9 \n\n");
+
     }
     
-    if (push == 2) {
-        posicaoJogo[0][1] = elemento;
+    if(push >= 1 && push <= 3) {
+        posicaoJogo[0][push - 1] = elemento;
     }
     
-    if (push == 3) {
-        posicaoJogo[0][2] = elemento;
+    else if(push >= 4 && push <= 6) {
+        posicaoJogo[1][push - 4] = elemento;
     }
     
-    if (push == 4) {
-        posicaoJogo[1][0] = elemento;
+    else if(push >= 7 && push <= 9) {
+        posicaoJogo[2][push - 7] = elemento;
     }
     
-    if (push == 5) {
-        posicaoJogo[1][1] = elemento;
+}
+
+int validar(char jogada) {
+    int venceu = 0;
+    int valida = 0;
+    int linha;
+    int coluna;
+    
+    for(linha = 0; linha < 3; linha++) {
+            for(coluna = 0; coluna < 3; coluna++) {
+                if (posicaoJogo[linha][coluna] == jogada) {
+                    valida ++;
+                }
+            }
+            if(valida == 3) {
+                venceu = 1;
+                break;
+            }
+            valida = 0;
+            printf("\n");
     }
     
-    if (push == 6) {
-        posicaoJogo[1][2] = elemento;
-    }
+    return venceu;
     
-    if (push == 7) {
-        posicaoJogo[2][0] = elemento;
-    }
-    
-    if (push == 8) {
-        posicaoJogo[2][1] = elemento;
-    }
-    
-    if (push == 9) {
-        posicaoJogo[2][2] = elemento;
-    }
 }
